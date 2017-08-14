85c66f3416401a49b60c8cbe4d577177214e1ca8
newshinely <newshinely@126.com>
2017-08-14 22:11:14 +0800

    20170814 off

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -1,3 +1,12 @@
+;; 设置默认编码格式
+(set-language-environment "UTF-8")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(modify-coding-system-alist 'process "*" 'utf-8)
+
 ;; 去掉警告提示音
 (setq ring-bell-function 'ignore)
 
