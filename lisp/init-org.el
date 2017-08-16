edc5fdc8e89641e935e19ba0a197632d19ad1085
newshinely <newshinely@126.com>
2017-08-16 18:57:13 +0800

    201708160ff

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -9,15 +9,28 @@
 (setq org-agenda-files '("~/.emacs.d/org"))
 (global-set-key (kbd "C-c a") 'org-agenda)
 
-;; 配置capture
+;; 配置 capture 
 (global-set-key (kbd "C-c c") 'org-capture)
+;; 配置 capture 的文件
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "~/.emacs.d/org/todo.org" "Tasks")
-	 "* TODO %?\n  %i\n  %a")
+	 "* TODO %?\n SCHEDULED: <%<%Y-%m-%d %a %H:%M>>")
         ("j" "Journal" entry (file+datetree "~/.emacs.d/org/journal.org")
 	 "* %?\nEntered on %U\n  %i\n  %a")))
 
-;;;end  GTD 配置
+;; 配置 TODO 关键字
+(setq org-todo-keywords
+           '((sequence "TODO(t)" "|" "DONE(d!)")
+             (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
+             (sequence "|" "CANCELED(c@/!)")))
+
+(setq org-tag-alist '((:startgroup . nil)
+                      ("@Office" . ?o)
+		      ("@Home" . ?h)
+                      (:endgroup . nil)
+                      ("Call" . ?c)
+		      ("Email" . ?e)))
+;;; end  GTD 配置
 
 ;; test code
 ;; (org-remember-insinuate)
@@ -30,5 +43,15 @@
 ;;	("Note" ?r "* %?\n %i\n %a" "~/Documents/Dropbox/0.GTD/note.org" )
 ;;	("Project" ?p "** %?\n %i\n %a" "~/Documents/Dropbox/0.GTD/project.org" %g) ))
 ;; (setq org-default-notes-file (concat org-directory "/inbox.org"))
+
+;; 配置 TODO 关键字的样式
+;; (setq org-todo-keyword-faces
+;;       '(("word" . (:background "red" :foreground "white" :weight bold))
+;; 	("stady" . (:background "white" :foreground "red" :weight bold))
+;; 	("xiuxian" . (:foreground "MediumBlue" :weight bold))
+;; 	("PENDING" . (:background "LightGreen" :foreground "gray" :weight bold))
+;; 	("TODO" . (:background "DarkOrange" :foreground "black" :weight bold))
+;; 	("DONE" . (:background "azure" :foreground "Darkgreen" :weight bold))
+;; 	("ABORT" . (:background "gray" :foreground "black")) ))
 
 (provide 'init-org)
